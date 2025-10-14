df5235a1efe2f288d3da9955b6dcff0fa085d760
GiovanniBezerra08 <giovannibezerra3@gmail.com>
2025-10-14 11:18:44 -0300

    V0

---
+++ shooterdowntop/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene("SceneNameHere");
+    }
+
+    public void LoadNextInBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+        
+    
+}
