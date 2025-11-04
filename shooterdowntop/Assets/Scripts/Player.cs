8636d76df5670601d6f2347d844fdaf3a5106d17
GiovanniBezerra08 <giovannibezerra3@gmail.com>
2025-11-04 11:40:22 -0300

    v9

--- shooterdowntop/Assets/Scripts/Player.cs
+++ shooterdowntop/Assets/Scripts/Player.cs
@@ -17,28 +17,48 @@ public class Player : Personagem
         animator = GetComponent<Animator>();
     }
 
+    
 
     void Update()
     {
+        andando = false;
+
+        if (arma.rotation.eulerAngles.z > -90 && arma.rotation.eulerAngles.z < 90)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+        if (arma.rotation.eulerAngles.z > -90 && arma.rotation.eulerAngles.z < 270)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position += new Vector3 (0, getVelocidade() * Time.deltaTime,0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.position -= new Vector3 (0, getVelocidade() * Time.deltaTime,0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3 (getVelocidade() * Time.deltaTime, 0 ,0);
+            andando = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3 (getVelocidade() * Time.deltaTime, 0 ,0);
+            andando = true;
         }
+        
+        animator.SetBool("andando", andando);
 
     }
 }
